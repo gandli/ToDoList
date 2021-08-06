7057e45b2df6580e6a4d971db9c0923f702849ea
gandli <gandli@qq.com>
2021-08-06 21:55:26 +0800

    格式化

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,57 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var UserData:ToDo = ToDo(data: [SingleToDo(title: "吃饭", duedate: Date()),
-                                    SingleToDo(title: "写作业", duedate: Date()),
-                                    SingleToDo(title: "运动", duedate: Date()),
-                                    SingleToDo(title: "睡觉", duedate: Date()),
-                            ])
-    
-    var body: some View {
-        ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: true){
-            VStack {
-                ForEach(self.UserData.ToDoList){item in
-                    SingleCardView(index: item.id)
-                        .environmentObject(self.UserData)
-                        .padding()
-                }
-            }
+  @ObservedObject var UserData: ToDo = ToDo(data: [
+    SingleToDo(title: "吃饭", duedate: Date()),
+    SingleToDo(title: "写作业", duedate: Date()),
+    SingleToDo(title: "运动", duedate: Date()),
+    SingleToDo(title: "睡觉", duedate: Date()),
+  ])
+
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: true) {
+      VStack {
+        ForEach(self.UserData.ToDoList) { item in
+          SingleCardView(index: item.id)
+            .environmentObject(self.UserData)
+            .padding()
         }
+      }
     }
+  }
 }
 
-struct SingleCardView:View {
-//    @State var isChecked:Bool = false
-    
-//    var title:String = ""
-//    var duedate:Date = Date()
-    @EnvironmentObject var UserData: ToDo
-    var index: Int
-    var body:some View{
-        HStack{
-            Rectangle().frame(width: 6).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            
-            VStack(alignment: .leading, spacing: 6.0, content: {
-                Text(self.UserData.ToDoList[index].title).font(.headline).fontWeight(.heavy)
-                Text(self.UserData.ToDoList[index].duedate.description).font(.subheadline)
-                    .foregroundColor(.gray)
-            }).padding(.leading)
-            
-            Spacer()
-            
-            Image(systemName:self.UserData.ToDoList[index].isChecked ? "checkmark.square.fill" : "square").imageScale(.large).padding(.trailing)
-                .onTapGesture {
-                    self.UserData.check(id: self.index)
-                }
-        }.frame(height:80)
-        .background(Color.white)
-        .cornerRadius(10)
-        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/,x:0,y:10 )
-    }
+struct SingleCardView: View {
+  @EnvironmentObject var UserData: ToDo
+  var index: Int
+
+  var body: some View {
+    HStack {
+      Rectangle().frame(width: 6).foregroundColor(
+        .blue)
+
+      VStack(
+        alignment: .leading, spacing: 6.0,
+        content: {
+          Text(self.UserData.ToDoList[index].title).font(.headline).fontWeight(.heavy)
+          Text(self.UserData.ToDoList[index].duedate.description).font(.subheadline)
+            .foregroundColor(.gray)
+        }
+      ).padding(.leading)
+
+      Spacer()
+
+      Image(
+        systemName: self.UserData.ToDoList[index].isChecked ? "checkmark.square.fill" : "square"
+      ).imageScale(.large).padding(.trailing)
+        .onTapGesture {
+          self.UserData.check(id: self.index)
+        }
+    }.frame(height: 80)
+      .background(Color.white)
+      .cornerRadius(10)
+      .shadow(radius: 10, x: 0, y: 10)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
