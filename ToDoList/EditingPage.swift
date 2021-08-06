5fb9e53013a8565cf65e338cdae29fbca317bd13
gandli <gandli@qq.com>
2021-08-06 23:11:15 +0800

    Editing

---
+++ ToDoList/EditingPage.swift
@@ -0,0 +1,20 @@
+//
+//  EditingPage.swift
+//  ToDoList
+//
+//  Created by g on 2021/8/6.
+//
+
+import SwiftUI
+
+struct EditingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        EditingPage()
+    }
+}
